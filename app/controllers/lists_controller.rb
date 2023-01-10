80b7d414085a11d463a2ba5f8d7dd40cdf4cb23c
EC2 Default User <ec2-user@ip-172-31-14-145.ap-northeast-1.compute.internal>
2023-01-10 12:14:37 +0000

    [Finish] 3-12

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,11 +3,11 @@ class ListsController < ApplicationController
     # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する
     @list = List.new
   end
-  
-  # 保存機能追加のために追記
+
   def create
     @list = List.new(list_params)
     if @list.save
+      flash[:notice] = "投稿が成功しました"
       redirect_to list_path(@list.id)
     else
       render :new
@@ -25,21 +25,21 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to list_path(list.id)
   end
-  
-  
+
+
   def destroy
     list = List.find(params[:id])
     list.destroy
     redirect_to '/lists'
   end
-  
-  
+
+
   private
   # ストロングパラメータ
   def list_params
